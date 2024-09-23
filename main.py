8e871f19828401596fa0997461dd211edef5ea11
Mei Zhang <rubymxzhang@gmail.com>
2024-09-23 18:58:20 +0800

    add logic to set record_a or record_b to be the next record to be compared

--- main.py
+++ main.py
@@ -10,27 +10,37 @@ def get_record(data):
 
 def format_data(account):
     """Takes the account data and returns the printable format."""
-    account_name=account["name"]
+    account_name = account["name"]
     account_descr = account["description"]
     account_country = account["country"]
-    return f"{account_name}, a {account_descr}, from {account['country']}."
+    return f"{account_name}, a {account_descr}, from {account_country}."
 
 #write a function to compare results
 def avoid_same_record(record_a, record_b):
+    against_b = print(f"Against B: {format_data(record_b)}.")
      # make sure there are no duplicate records
     if record_a==record_b:
         record_b = get_record(data)  
-        print(f"Against B: {format_data(record_b)}.")
-
+        against_b
     else:
-         print(f"Against B: {format_data(record_b)}.")
+        against_b
 
 current_score=0
 final_score=0
+next_record_a=None
+next_record_b=None
+
 
 while True:
-    record_a = get_record(data)  
-    #print(record_a)
+    if next_record_a is not None:
+        record_a=next_record_a
+        next_record_a=None
+    elif next_record_b is not None:
+        record_a=next_record_b
+        next_record_b=None
+    else:
+        record_a = get_record(data)  
+        #print(record_a)
     print(f"Compare A: {format_data(record_a)}.")
     print(vs)
     record_b = get_record(data)  
@@ -42,6 +52,8 @@ while True:
         if record_a["follower_count"] > record_b["follower_count"]:
             current_score+=1
             print(f"Congratulations! You are correct. {record_a['name']} has more followers than {record_b['name']}.  Current score: {current_score}")
+            #set record_a to be the next record to be compared
+            next_record_a=record_a
         else:
             print(logo)
             final_score=current_score
@@ -54,6 +66,8 @@ while True:
             print("\n")
             print(f"Congratulations! You are correct. {record_b['name']} has more followers than {record_a['name']}.  Current score: {current_score}")
             print("\n")
+            #set record_b to be the next record to be compared
+            next_record_b=record_b
         else:
             print(logo)
             final_score=current_score
